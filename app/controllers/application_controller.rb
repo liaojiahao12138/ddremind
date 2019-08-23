723b11e0bb181ce1a0cd1c1aaff70067d6c7fc88
huminghua-86 <huminghua-86@163.com>
2019-08-23 03:22:54 +0000

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-    render html: "this is dd_remind !"
-  end
+  
+  include SessionsHelper
 end
